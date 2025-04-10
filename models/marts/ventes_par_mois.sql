e816bc807040403c0e0de35c735533ec90ea7b74
Hugues Bousquet <hbsqt@posteo.net>
2025-04-10 15:12:07 +0000

    Awesome commit message

--- models/marts/ventes_par_mois.sql
+++ models/marts/ventes_par_mois.sql
@@ -1,7 +1,7 @@
 SELECT 
     d.annee,
     d.mois,
-    SUM(v.montant_total) AS ventes_par_mois
+    ROUND(SUM(v.montant_total), 2) AS ventes_par_mois
 FROM {{ source('TechStore', 'Ventes') }} v
     JOIN {{ ref("stg_dates") }} d USING(id_date)
 GROUP BY d.annee, d.mois
